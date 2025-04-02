066cfc4883c8366779276ad78b729b856bb51e16
AshwiniVeddamoni <Ashwiniveddamoni@gmail.com>
2025-04-01 20:35:49 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from "./components/NavBar/navbar";
+import About from "./components/About/about";
+import Experience from "./components/Experience/experience";
+import Education from "./components/Education/education";
+import Skills from "./components/Skills/skills";
+import Projects from "./components/Projects/projects";
+
+
+const App = () => {
+    return (
+        <div className="app-container">
+            <Navbar />
+            <div className="content">
+                <About />
+                <Experience />
+                <Education />
+                <Skills />
+                <Projects />
+            </div>
+        </div>
+    );
+};
+
+export default App;
